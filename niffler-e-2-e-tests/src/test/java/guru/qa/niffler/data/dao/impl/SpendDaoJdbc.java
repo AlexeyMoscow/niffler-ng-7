04cbe93877499c8efbcd585e9d91246f7fa2c533
Alexey <alex.nowik@mail.ru>
2025-01-19 11:55:29 +0300

    Homework 5.1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/SpendDaoJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/SpendDaoJdbc.java
@@ -2,13 +2,17 @@ package guru.qa.niffler.data.dao.impl;
 
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.data.dao.SpendDao;
+import guru.qa.niffler.data.entity.spend.CategoryEntity;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
+import guru.qa.niffler.model.CurrencyValues;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class SpendDaoJdbc implements SpendDao {
@@ -50,5 +54,31 @@ public class SpendDaoJdbc implements SpendDao {
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  @Override
+  public List<SpendEntity> findAll() {
+    List<SpendEntity> spends = new ArrayList<>();
+    try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM spend");
+         ResultSet rs = ps.executeQuery()) {
+      while (rs.next()) {
+        SpendEntity spend = new SpendEntity();
+        spend.setId(rs.getObject("id", UUID.class));
+        spend.setUsername(rs.getString("username"));
+        spend.setSpendDate(rs.getDate("spend_date"));
+        spend.setCurrency(CurrencyValues.valueOf(rs.getString("currency")));
+        spend.setAmount(rs.getDouble("amount"));
+        spend.setDescription(rs.getString("description"));
+
+        CategoryEntity category = new CategoryEntity();
+        category.setId(rs.getObject("category_id", UUID.class));
+        spend.setCategory(category);
+
+        spends.add(spend);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return spends;
   }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -2,11 +2,18 @@ package guru.qa.niffler.service;
 
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
+import guru.qa.niffler.data.dao.impl.CategoryDaoSpringJdbc;
 import guru.qa.niffler.data.dao.impl.SpendDaoJdbc;
+import guru.qa.niffler.data.dao.impl.SpendDaoSpringJdbc;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
+import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static guru.qa.niffler.data.Databases.dataSource;
 import static guru.qa.niffler.data.Databases.transaction;
 
 public class SpendDbClient {
@@ -28,4 +35,26 @@ public class SpendDbClient {
         CFG.spendJdbcUrl()
     );
   }
+
+    public List<SpendJson> findAllSpendsSpring() {
+        return transaction(connection -> {
+                    List<SpendEntity> spendEntities = new SpendDaoSpringJdbc(dataSource(CFG.spendJdbcUrl())).findAll();
+                    return spendEntities.stream()
+                            .map(SpendJson::fromEntity)
+                            .collect(Collectors.toList());
+                },
+                CFG.spendJdbcUrl()
+        );
+    }
+
+    public List<CategoryJson> findAllCategoriesSpring() {
+        return transaction(connection -> {
+                    List<CategoryEntity> categoryEntities = new CategoryDaoSpringJdbc(dataSource(CFG.spendJdbcUrl())).findAll();
+                    return categoryEntities.stream()
+                            .map(CategoryJson::fromEntity)
+                            .collect(Collectors.toList());
+                },
+                CFG.spendJdbcUrl()
+        );
+    }
 }
